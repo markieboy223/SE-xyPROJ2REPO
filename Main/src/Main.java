37d8fd207e173b9416ed6261f92f9c855acf89b4
markieboy223 <markvanderburg01@gmail.com>
2023-05-11 09:50:29 +0200

    field

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,26 +3,32 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        TextField field = new TextField();
+        field.setText("Voer hier uw naampie in");
+
         Button btn = new Button();
         btn.setText("Mogge");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("hallo");
+                System.out.println("hallo " + field.getText() + "!");
             }
         });
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        BorderPane root = new BorderPane();
+        root.setBottom(btn);
 
-        Scene scene = new Scene(root, 30, 250);
+        root.setTop(field);
 
-        primaryStage.setTitle("test");
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
