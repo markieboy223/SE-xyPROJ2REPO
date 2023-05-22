9082770d952de1fe8cec00c42888a10aed037a44
Bartvb0 <21057788@student.hhs.nl>
2023-05-22 16:53:10 +0200

    Verder gewerkt aan het antwoord

--- src/main/java/com/example/Project2/chatController.java
+++ src/main/java/com/example/Project2/chatController.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class chatController {
     @FXML
@@ -20,46 +21,62 @@ public class chatController {
     private Label outputTekst;
     @FXML
     private Button sendButton;
+    private ArrayList<String> tabellen = new ArrayList<>();
 
     public void SendButtonOnAction(ActionEvent event){
+        DatabaseConnection connection = new DatabaseConnection();
         String input = inputTekst.getText();
         String onderwerp = vindOnderwerp(input);
-        StringBuilder tableCheck = new StringBuilder();
         StringBuilder antwoord = new StringBuilder();
-        String query = onderwerp;
 
+        if (onderwerp != null){
+            try (Connection connectDB = connection.getConnection2()){
+                PreparedStatement statement = connectDB.prepareStatement("SELECT * FROM " + onderwerp);
+
+                try (ResultSet queryResult = statement.executeQuery()) {
+                    while (queryResult.next()){
+                        antwoord.append(queryResult.getString(onderwerp));
+                        antwoord.append(" ");
+                    }
+                    outputTekst.setText("Q: " + input + "\n" + "A: " + antwoord.toString() + "\n");
+                    inputTekst.clear();
+                }
+            }
+            catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if(onderwerp == null){
+            String help = "Hier heb ik geen informatie over. \nIk heb alleen kennis over de volgende onderwerpen: \n";
+            String help2 = "";
+            for (String dit : tabellen){
+                help2 =  help2 + dit + "\n";
+            }
+            help = help + help2;
+            outputTekst.setText(help);
+        }
+
+    }
+    public String vindOnderwerp(String input){
         DatabaseConnection connection = new DatabaseConnection();
-        try (Connection connectDB = connection.getConnection2()){
-            PreparedStatement tableNames = connectDB.prepareStatement("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = " + query + "WHERE TABLE_SCHEMA = 'documentatie'");
-            PreparedStatement statement = connectDB.prepareStatement("SELECT * FROM " + query);
+        StringBuilder tableCheck = new StringBuilder();
+        String [] apart = input.split(" ");
 
+        try (Connection connectDB = connection.getConnection2()){
+            PreparedStatement tableNames = connectDB.prepareStatement("SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'documentatie'");
             try (ResultSet queryResult2 = tableNames.executeQuery()){
                 while (queryResult2.next()){
                     tableCheck.append(queryResult2.getString(1));
-                    System.out.println(tableCheck);
+                    tabellen.add(String.valueOf(tableCheck));
+                    tableCheck.setLength(0);
                 }
-            }
-            try (ResultSet queryResult = statement.executeQuery()) {
-                while (queryResult.next()){
-                    antwoord.append(queryResult.getString("kosten"));
-                    antwoord.append(" ");
-                }
-                outputTekst.setText(antwoord.toString());
-                System.out.println("test");
             }
         }
         catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
-        if(onderwerp == null){
-            outputTekst.setText("Hier heb ik geen informatie over.");
-        }
-        //else{outputTekst.setText("Uw gekozen onderwerp is " + onderwerp + ".");}
-    }
-    public String vindOnderwerp(String input){
-        String[] tabellen = {"inkomen", "kosten", "medewerkers"};
-        String[] apart = input.split(" ");
         for (String woord : apart){
             for (String tabel : tabellen){
                 if (woord.toLowerCase().equals(tabel)){
@@ -67,6 +84,7 @@ public class chatController {
                 }
             }
         }
+
         return null;
     }
 
